429387e3e01812e1c1e7becf18f339e4c3e3eb3b
Jacog712 <giraldozuluagajm@gmail.com>
2023-02-18 14:29:10 -0500

    Instalacion y configuracion de ejs

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('views', './views/')
 
 
 app.use(logger('dev'));
