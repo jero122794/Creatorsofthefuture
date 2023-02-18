6478c82f075c3d4a5b66f829720d8fc1bed49a4a
Dacmay29 <dacmay29@outlook.com>
2023-02-17 21:03:37 -0500

    actualizado rango activo

--- creatorsofthefuture.sql
+++ creatorsofthefuture.sql
@@ -36,6 +36,7 @@ CREATE TABLE `usuarios` (
   `cv_url` varchar(255) DEFAULT NULL,
   `views_count` int DEFAULT NULL,
   `last_viewed` datetime DEFAULT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -46,7 +47,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (1,'Juan','Pérez','juan.perez@gmail.com','5551234567','Desarrollador','Experto en programación web','https://example.com/juanperez.jpg','Licenciatura en Informática','https://www.linkedin.com/in/juanperez','https://example.com/cv_juanperez.pdf',10,'2023-02-15 10:30:00'),(2,'María','García','maria.garcia@gmail.com','5552345678','Diseñadora','Especialista en diseño gráfico','https://example.com/mariagarcia.jpg','Licenciatura en Diseño Gráfico','https://www.linkedin.com/in/mariagarcia','https://example.com/cv_mariagarcia.pdf',5,'2023-02-14 15:45:00'),(3,'Pedro','López','pedro.lopez@gmail.com','5553456789','Gerente de Proyectos','Experiencia en liderazgo de equipos','https://example.com/pedrolopez.jpg','Maestría en Administración de Empresas','https://www.linkedin.com/in/pedrolopez','https://example.com/cv_pedrolopez.pdf',8,'2023-02-13 20:00:00'),(4,'Ana','Martínez','ana.martinez@gmail.com','5554567890','Analista de Datos','Especialista en minería de datos','https://example.com/anamartinez.jpg','Maestría en Ciencia de Datos','https://www.linkedin.com/in/anamartinez','https://example.com/cv_anamartinez.pdf',3,'2023-02-12 11:15:00'),(5,'Carlos','Ramírez','carlos.ramirez@gmail.com','5555678901','Ingeniero de Software','Experto en desarrollo de aplicaciones móviles','https://example.com/carlosramirez.jpg','Licenciatura en Ingeniería de Sistemas','https://www.linkedin.com/in/carlosramirez','https://example.com/cv_carlosramirez.pdf',7,'2023-02-11 16:30:00'),(6,'Laura','González','laura.gonzalez@gmail.com','5556789012','Especialista en Marketing','Conocimiento en estrategias de marketing digital','https://example.com/lauragonzalez.jpg','Maestría en Marketing Digital','https://www.linkedin.com/in/lauragonzalez','https://example.com/cv_lauragonzalez.pdf',2,'2023-02-10 21:45:00');
+INSERT INTO `usuarios` VALUES (1,'Juan','Pérez','juan.perez@gmail.com','5551234567','Desarrollador','Experto en programación web','https://example.com/juanperez.jpg','Licenciatura en Informática','https://www.linkedin.com/in/juanperez','https://example.com/cv_juanperez.pdf',10,'2023-02-15 10:30:00',1),(2,'María','García','maria.garcia@gmail.com','5552345678','Diseñadora','Especialista en diseño gráfico','https://example.com/mariagarcia.jpg','Licenciatura en Diseño Gráfico','https://www.linkedin.com/in/mariagarcia','https://example.com/cv_mariagarcia.pdf',5,'2023-02-14 15:45:00',0),(3,'Pedro','López','pedro.lopez@gmail.com','5553456789','Gerente de Proyectos','Experiencia en liderazgo de equipos','https://example.com/pedrolopez.jpg','Maestría en Administración de Empresas','https://www.linkedin.com/in/pedrolopez','https://example.com/cv_pedrolopez.pdf',8,'2023-02-13 20:00:00',0),(4,'Ana','Martínez','ana.martinez@gmail.com','5554567890','Analista de Datos','Especialista en minería de datos','https://example.com/anamartinez.jpg','Maestría en Ciencia de Datos','https://www.linkedin.com/in/anamartinez','https://example.com/cv_anamartinez.pdf',3,'2023-02-12 11:15:00',0),(5,'Carlos','Ramírez','carlos.ramirez@gmail.com','5555678901','Ingeniero de Software','Experto en desarrollo de aplicaciones móviles','https://example.com/carlosramirez.jpg','Licenciatura en Ingeniería de Sistemas','https://www.linkedin.com/in/carlosramirez','https://example.com/cv_carlosramirez.pdf',7,'2023-02-11 16:30:00',0),(6,'Laura','González','laura.gonzalez@gmail.com','5556789012','Especialista en Marketing','Conocimiento en estrategias de marketing digital','https://example.com/lauragonzalez.jpg','Maestría en Marketing Digital','https://www.linkedin.com/in/lauragonzalez','https://example.com/cv_lauragonzalez.pdf',2,'2023-02-10 21:45:00',0);
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -63,4 +64,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-02-15 22:20:09
+-- Dump completed on 2023-02-17 21:01:14
